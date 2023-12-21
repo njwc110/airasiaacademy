c2e5d1afe26816123c43aa4508cd8399527f468b
njwc110 <129952870+njwc110@users.noreply.github.com>
2023-12-21 14:07:39 +0800

    Update iris_prediction_model.py

--- iris_prediction_model.py
+++ iris_prediction_model.py
@@ -1,7 +1,7 @@
 import streamlit as st  
 import pandas as pd
 import seaborn as sns
-from sklearn.svm
+from sklearn.svm import SVC
 
 st.write("# Simple Iris Flower Prediction App")
 st.write("This app predicts the **Iris flower** type!")
